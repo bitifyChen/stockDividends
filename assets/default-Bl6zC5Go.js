4b1bc7477f42091d8352b73541c49071841f0ddf
bitifyChen <bitifyChen@users.noreply.github.com>
2024-07-04 12:34:00 +0000

    deploy: 293dfb167318425452f96b2deacc30332abe132c

---
+++ assets/default-Bl6zC5Go.js
@@ -0,0 +1,1 @@
+import{_ as t,o as a,c as r,a as e,b as s,F as c,r as n}from"./index-uwXGa9Rs.js";const _={},l=e("header",null,"header",-1),d={class:"base-wrap flex-1"};function f(i,u){const o=n("router-view");return a(),r(c,null,[l,e("div",d,[s(o)])],64)}const p=t(_,[["render",f]]);export{p as default};
