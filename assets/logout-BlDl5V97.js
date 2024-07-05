73e32c0efcf0403166596ec16250f9cc1bbaf425
bitifyChen <bitifyChen@users.noreply.github.com>
2024-07-05 10:00:25 +0000

    deploy: 9a454f68c3883452ac887cd8fc3508c8a534e9de

---
+++ assets/logout-BlDl5V97.js
@@ -0,0 +1,1 @@
+import{R as t,L as s,M as o}from"./index-DBWs3uuA.js";const n={__name:"logout",setup(r){const e=t();return s(["token"]).remove("token"),e.push({name:"LoginPage"}),(c,a)=>null}};typeof o=="function"&&o(n);export{n as default};
