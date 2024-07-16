38616787e228a396090743cbc8fc01d995ec9ec2
Kevin.Chen <kevin.,c>
2024-07-16 17:11:51 +0800

    feat:firebase(dividend)

--- src/api/price.js
+++ src/api/price.js
@@ -1,3 +1,4 @@
 import request from '@/api/useSheetApi.js'
 
 export const getPrice = (params) => request('get', 'price', null, params)
+export const postPrice = (id) => request('post', 'price', { stockId: `'${id}` }, null)
