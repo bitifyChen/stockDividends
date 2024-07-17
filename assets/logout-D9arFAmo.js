5f95f3eb0e5eba3166a98dc1ee1811ea53653904
bitifyChen <bitifyChen@users.noreply.github.com>
2024-07-17 02:19:04 +0000

    deploy: 9d36c42bcb483aa18159b781b9a3330a530bbc5b

---
+++ assets/logout-D9arFAmo.js
@@ -0,0 +1,1 @@
+import{$ as n,a0 as r,Q as c,a1 as u,a2 as i,R as o}from"./index-Ds7PqTxV.js";const p={__name:"logout",setup(l){const e=n(),s=r(["token"]),t=c(),a=u(0);return s.remove("token"),t.clear(),a.clear(),i().finally(()=>{e.push({name:"LoginPage"})}),(f,_)=>null}};typeof o=="function"&&o(p);export{p as default};
