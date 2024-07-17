439f5324a446daa721f429b32f5ff60d7afd0052
Kevin.Chen <kevin.,c>
2024-07-17 17:45:34 +0800

    Fix/drone error

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,9 @@
 <script setup></script>
 <template>
-  <!-- <div id="bgWrap"></div> -->
   <router-view class="base-wrap" />
 </template>
 
 <style scoped>
-#bgWrap {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  background-image: url('@/assets/image/base/texture.png');
-  -webkit-transition: background-color 2s ease-in;
-}
 .base-wrap {
   position: relative;
   z-index: 1;
