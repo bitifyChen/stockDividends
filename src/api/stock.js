e8e9a0d58ef380fe1c101360c71264d82e8171e7
K.Chen <43808882+bitifyChen@users.noreply.github.com>
2024-07-20 10:21:46 +0800

    Remove/API_KEY

--- src/api/stock.js
+++
@@ -1,7 +0,0 @@
-import request from '@/api/useSheetApi.js'
-
-export const getStock = (params) => request('get', 'stock', null, params)
-
-export const postStock = (params) => request('post', 'stock', params)
-
-export const getStockDividend = (params) => request('get', 'stockDividend', null, params)
--- src/api/user.js
+++
@@ -1,5 +0,0 @@
-import request from '@/api/useSheetApi.js'
-
-export const getUser = (params) => request('get', 'user', null, params)
-
-export const postUser = (params) => request('post', 'user', params)
