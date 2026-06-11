cc33fb9bc04598dbd86d090056207b785320e806
bitifyChen <bitifyChen@users.noreply.github.com>
2026-06-11 08:34:35 +0000

    deploy: 6c2b95ab53e7145f7f4b6ae442b0781e042f2c7d

--- assets/_...all_-1i6VSvRJ.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,O as _}from"./index-BVx6Qn8b.js";const c={};function e(n,o){return"404.ERROR"}typeof _=="function"&&_(c);const f=t(c,[["render",e]]);export{f as default};
---
+++ assets/useDashboardSetting-tZHjJtBX.js
@@ -0,0 +1,1 @@
+import{aO as e}from"./index-Bg_R7A6I.js";const a=e("dashboardSetting",{persist:!0,state:()=>({shareUnit:"share"}),actions:{setShareUnit(t){this.shareUnit=t==="lot"?"lot":"share"}}});export{a as u};
