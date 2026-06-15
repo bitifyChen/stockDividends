fcbd73311f5deade1fdf438cb2b2d84291866c3c
bitifyChen <bitifyChen@users.noreply.github.com>
2026-06-15 09:55:50 +0000

    deploy: 020230b0fd4a710751b15e472f3d170c1582ef1c

---
+++ assets/useDashboardSetting-BFYxB-TK.js
@@ -0,0 +1,1 @@
+import{aV as e}from"./index-WeqI1SXI.js";const a=e("dashboardSetting",{persist:!0,state:()=>({shareUnit:"share"}),actions:{setShareUnit(t){this.shareUnit=t==="lot"?"lot":"share"}}});export{a as u};
