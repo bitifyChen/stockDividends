2a39cfc8f5f926658dcd94c26cdac14343ca0ee3
bitifyChen <bitifyChen@users.noreply.github.com>
2026-07-03 16:43:45 +0000

    deploy: dec6a3c10abdedb7aa3d75199774a4c9e21843d0

---
+++ assets/useDashboardSetting-tSKCWQMH.js
@@ -0,0 +1,1 @@
+import{b5 as t}from"./index-E9Q6C9Cv.js";const r=t("dashboardSetting",{persist:!0,state:()=>({shareUnit:"share",themeMode:"dark"}),actions:{setShareUnit(e){this.shareUnit=e==="lot"?"lot":"share"},setThemeMode(e){this.themeMode=["light","dark","system"].includes(e)?e:"dark"}}});export{r as u};
