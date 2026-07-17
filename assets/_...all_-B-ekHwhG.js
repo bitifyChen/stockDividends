a287511b50888df5de4fce892283933441110b17
bitifyChen <bitifyChen@users.noreply.github.com>
2026-07-17 02:20:44 +0000

    deploy: 928f901fee24a2be1dda09ca55d4089b065df734

---
+++ assets/_...all_-B-ekHwhG.js
@@ -0,0 +1,6 @@
+import{_,q as n,e as d,f as l,g as e,h as p,i,a6 as t,y as h,z as u}from"./index--S7xcBE0.js";import{c as f}from"./createLucideIcon-C6C09q32.js";/**
+ * @license lucide-vue-next v0.419.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const m=f("HouseIcon",[["path",{d:"M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8",key:"5wwlr5"}],["path",{d:"M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z",key:"1d0kgt"}]]),s=o=>(h("data-v-aac24b8e"),o=o(),u(),o),k={class:"error-container"},v={class:"error-content"},I=s(()=>e("h1",{class:"error-code"},"404",-1)),b=s(()=>e("h2",{class:"error-title"},"找不到頁面",-1)),g=s(()=>e("p",{class:"error-message"},"抱歉，您訪問的頁面可能已被移除、名稱已更改或暫時無法使用。",-1)),y=s(()=>e("span",null,"回到首頁",-1)),a={__name:"[...all]",setup(o){const c=n(),r=()=>c.push("/");return(H,w)=>(d(),l("div",k,[e("div",v,[I,b,g,e("button",{class:"home-button",onClick:r},[p(i(m),{size:18}),y])])]))}};typeof t=="function"&&t(a);const B=_(a,[["__scopeId","data-v-aac24b8e"]]);export{B as default};
